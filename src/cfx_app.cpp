f984e263215d64d7c4e3bc91af0a019b35b4d8a2
Zakhrov <aaron.zakhrov@gmail.com>
2021-05-25 01:27:05 +0530

    add window resize

--- src/cfx_app.cpp
+++ src/cfx_app.cpp
@@ -7,7 +7,7 @@ namespace cfx{
     App::App(){
         loadModels();
         createPipelineLayout();
-        createPipeline();
+        recreateSwapChain();
         createCommandBuffers();
     }
     App::~App(){
@@ -62,16 +62,26 @@ namespace cfx{
         }
     }
     void App::createPipeline(){
-        auto pipelineConfig = CFXPipeLine::defaultPipelineConfigInfo(cfxSwapChain.width(),cfxSwapChain.height());
-        pipelineConfig.renderPass = cfxSwapChain.getRenderPass();
+        auto pipelineConfig = CFXPipeLine::defaultPipelineConfigInfo(cfxSwapChain->width(),cfxSwapChain->height());
+        pipelineConfig.renderPass = cfxSwapChain->getRenderPass();
         pipelineConfig.pipelineLayout = pipelineLayout;
         cfxPipeLine = std::make_unique<CFXPipeLine>(cfxDevice,pipelineConfig,
         "shaders/simple_shader.vert.spv",
         "shaders/simple_shader.frag.spv");
     }
+    void App::recreateSwapChain(){
+        auto extent = window.getExtent();
+        while(extent.width == 0 || extent.height == 0){
+            extent = window.getExtent();
+            glfwWaitEvents();
+        }
+        vkDeviceWaitIdle(cfxDevice.device());
+        cfxSwapChain = std::make_unique<CFXSwapChain>(cfxDevice,extent);
+        createPipeline();
+    }
 
      void App::createCommandBuffers(){
-         commandBuffers.resize(cfxSwapChain.imageCount());
+         commandBuffers.resize(cfxSwapChain->imageCount());
          VkCommandBufferAllocateInfo allocInfo{};
          allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
          allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
@@ -81,20 +91,24 @@ namespace cfx{
          if(vkAllocateCommandBuffers(cfxDevice.device(),&allocInfo,commandBuffers.data()) != VK_SUCCESS){
              throw std::runtime_error("failed to allocate command buffers");
          }
-         for(int i=0; i<commandBuffers.size(); i++){
-             VkCommandBufferBeginInfo beginInfo{};
+        
+
+
+     }
+     void App::recordCommandBuffer(int imageIndex){
+           VkCommandBufferBeginInfo beginInfo{};
              beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
 
-             if(vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS){
+             if(vkBeginCommandBuffer(commandBuffers[imageIndex], &beginInfo) != VK_SUCCESS){
                  throw std::runtime_error("failed to begin command buffer");
              }
 
              VkRenderPassBeginInfo renderPassInfo{};
              renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-             renderPassInfo.renderPass = cfxSwapChain.getRenderPass();
-             renderPassInfo.framebuffer = cfxSwapChain.getFrameBuffer(i);
+             renderPassInfo.renderPass = cfxSwapChain->getRenderPass();
+             renderPassInfo.framebuffer = cfxSwapChain->getFrameBuffer(imageIndex);
              renderPassInfo.renderArea.offset = {0,0};
-             renderPassInfo.renderArea.extent = cfxSwapChain.getSwapChainExtent();
+             renderPassInfo.renderArea.extent = cfxSwapChain->getSwapChainExtent();
 
              std::array<VkClearValue, 2> clearValues{};
              clearValues[0].color = {0.1f,0.1f,0.1f,1.0f};
@@ -102,34 +116,37 @@ namespace cfx{
              renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
              renderPassInfo.pClearValues = clearValues.data();
 
-             vkCmdBeginRenderPass(commandBuffers[i],&renderPassInfo,VK_SUBPASS_CONTENTS_INLINE);
+             vkCmdBeginRenderPass(commandBuffers[imageIndex],&renderPassInfo,VK_SUBPASS_CONTENTS_INLINE);
 
-             cfxPipeLine->bind(commandBuffers[i]);
-            cfxModel->bind(commandBuffers[i]);
-            cfxModel->draw(commandBuffers[i]);
+             cfxPipeLine->bind(commandBuffers[imageIndex]);
+            cfxModel->bind(commandBuffers[imageIndex]);
+            cfxModel->draw(commandBuffers[imageIndex]);
 
-             vkCmdEndRenderPass(commandBuffers[i]);
-             if(vkEndCommandBuffer(commandBuffers[i])!=VK_SUCCESS){
+             vkCmdEndRenderPass(commandBuffers[imageIndex]);
+             if(vkEndCommandBuffer(commandBuffers[imageIndex])!=VK_SUCCESS){
                  throw std::runtime_error("failed to record command buffer");
              }
-
-
-
-
-
-         }
-
-
      }
         void App::drawFrame(){
             uint32_t imageIndex;
-            auto result = cfxSwapChain.acquireNextImage(&imageIndex);
+            auto result = cfxSwapChain->acquireNextImage(&imageIndex);
+            if(result == VK_ERROR_OUT_OF_DATE_KHR){
+                recreateSwapChain();
+                return;
+            }
 
             if(result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR){
                 throw std::runtime_error("failed to aquire swap chain image");
             }
+            recordCommandBuffer(imageIndex);
 
-            result = cfxSwapChain.submitCommandBuffers(&commandBuffers[imageIndex],&imageIndex);
+            result = cfxSwapChain->submitCommandBuffers(&commandBuffers[imageIndex],&imageIndex);
+            if(result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || window.wasWindowResized()){
+                window.restWindowResizedFlag();
+                recreateSwapChain();
+                return;
+
+            }
             if(result != VK_SUCCESS){
                 throw std::runtime_error("failed to present swap chain image");
             }
